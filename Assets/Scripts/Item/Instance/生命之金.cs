ebab2c3acb926a47de254e7383bb7a2e76621877
陈语奇 <504275297@qq.com>
2024-08-17 19:52:13 +0800

    生命之金道具修改

--- Assets/Scripts/Item/Instance/生命之金.cs
+++ Assets/Scripts/Item/Instance/生命之金.cs
@@ -1,4 +1,4 @@
-// 将所有的金币1：2转换为最大生命。
+// 将所有的金币1：2转换为最大生命。(无尽模式改为1:0.1)
 public class 生命之金 : BaseItem
 {
     public override int ID => 96;
@@ -9,7 +9,15 @@ public class 生命之金 : BaseItem
         base.OnGet();
         int val = GameManager.Instance.gameData.money;
         GameManager.Instance.gameData.money = 0;
-        attr.最大生命 += 2 * val;
+        if (GameManager.Instance.isEndlessMode())
+        {
+            attr.最大生命 += 0.1f * val;
+        }
+        else
+        {
+            attr.最大生命 += 2 * val;
+        }
+        
     }
 
 }
